e2e8dabcea60dbe58fef435e6b60b2b0ed63f28a
Raketensofa <raketensofa@hotmail.com>
2016-09-10 00:50:48 +0200

    10.09.2016 00:50

---
+++ RSSReader/RSSReader/Parser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+
+
+namespace RSSReader
+{
+    public class Parser
+    {
+
+
+
+
+
+
+        public void parseFeed(string feedUrl)
+        {
+
+            XmlReader reader = XmlReader.Create(feedUrl);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+            reader.Close();
+
+            foreach (SyndicationItem item in feed.Items)
+            {
+               
+                string subject = item.Title.Text;
+                string summary = item.Summary.Text;
+                string id = item.Id;
+            
+
+            }
+
+        }
+
+
+
+
+}
+}
